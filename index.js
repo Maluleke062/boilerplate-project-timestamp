ccfa9ed0d2a0749c15fad3245e02c6a49be6a2bb
ALVIN NTSAKO MALULEKE <105872985+Maluleke062@users.noreply.github.com>
2024-07-08 20:04:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
+// serving static assets
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// home page routing
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// returns current timestamp, if the date_string is empty
+app.get('/api', (req, res) => {
+  res.json(
+    {
+      "unix": new Date().getTime(),
+      "utc": new Date().toUTCString()
+    }
+  );
+});
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// JSON for other requests
+app.get('/api/:date_string', (req, res) => {
+  const {date_string} = req.params;
+  let date = new Date(date_string);
+
+  if (date.toString() === 'Invalid Date') {
+    date = new Date(parseInt(date_string));
+  }
+  if (date.toString() === 'Invalid Date') {
+    return res.json(
+      {
+        "error": "Invalid Date"
+      }
+    );
+  }
+  else {
+    return res.json(
+      {
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+      }
+    );
+  }
 });
 
 
+// app.listen(8000, () => {
+//   console.log("Your app is listening on port 8000...");
+// });
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
